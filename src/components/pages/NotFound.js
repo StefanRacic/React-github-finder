837bcb3a550f39146798055efbae0941fbe94fe9
Stefan Racic <stefracic@gmail.com>
2019-08-19 16:53:41 +0200

    Initial commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Oops not found</h1>
+      <p className="lead">Page that you are looking for does not exist</p>
+    </div>
+  );
+};
+
+export default NotFound;
